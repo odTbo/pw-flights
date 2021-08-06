c3b49a5d9d6b2b74799bcd93b852b59db6c88f8c
Vladimir <htcvlado@gmail.com>
2021-08-06 15:16:11 +0200

    TODO Get all possible flights

--- solution.py
+++ solution.py
@@ -8,7 +8,7 @@ class FlightSearch:
         self.selected_flights = []
         self.origin_airport = ""
         self.destination_airport = ""
-        self.num_bags = ""
+        # self.num_bags = ""
 
     def run(self):
         self.setup()
@@ -16,8 +16,10 @@ class FlightSearch:
         self.find_flight(self.origin_airport, self.destination_airport)
         pprint(self.selected_flights)
 
+    # Get the user input of Origin and Destination airport
     def setup(self):
         print("[Flight Search] Enter flight details.")
+
         self.origin_airport = str(input("\tOrigin airport: ")).upper()
         if len(self.origin_airport) == 0:
             raise Exception("Origin airport expected.")
@@ -31,30 +33,29 @@ class FlightSearch:
         # if :
         #     raise Exception("")
 
+    # Fetch flights from CSV to dict all_flights
     def fetch_flights(self):
         # Open flights data sheet
         with open("example/example0.csv") as f:
             file_data = csv.reader(f)
             headers = next(file_data)
             self.all_flights = [dict(zip(headers, i)) for i in file_data]
-        # pprint(self.all_flights)
 
+    # TODO Get correct flights
     def find_flight(self, origin, destination):
+        # Loops through flights and looks for a flight that fits both criteria (OA and DA)
         for flight in self.all_flights:
             if flight["origin"] == origin and flight["destination"] == destination:
                 self.selected_flights.append(flight)
-                return
 
+        # Loops through flights and looks for flight with correct OA
         for flight in self.all_flights:
             if flight["origin"] == origin:
                 self.selected_flights.append(flight)
+                # Get function where OA is DA of chosen flight
                 return self.find_flight(flight["destination"], destination)
 
 
 if __name__ == "__main__":
     flight_search = FlightSearch()
     flight_search.run()
-
-# for flight in self.all_flights:
-#     if flight["origin"] == origin and flight["destination"] == destination:
-#         self.selected_flights.append(flight)
